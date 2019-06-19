45c98033924d9bc9cea00fc32e763b461f4d152f
Nabuduwa-ketty <nabuduwaketty@gmail.com>
2019-06-19 12:46:34 +0300

    prime number

---
+++ getPrimeNumber.js
@@ -0,0 +1,20 @@
+function getPrimeNumber(max){
+var prime =[];
+var newPrime = [];
+var num;
+var i;
+var q = math.floor(Math.srt(n));
+for (num = 2; num <= max; ++num) 
+{
+	if (num%) {
+	prime.push(num)
+	}
+	if (num%2==0){
+	prime.push(num)
+	}
+	for (var i = 2;  i < num; i++) {
+
+      if (num % i !== 0 ) {
+      prime.push(getPrimeNumber)
+      }
+}
